7c1b0d63696f46404e4c0256beb9a7ff3881a559
emanFriendTales <eman@friendtales.com>
2023-09-29 13:18:02 +0500

    added CRUD operations for USER

--- mern-exercise-tracker/backend/routes/users.js
+++ mern-exercise-tracker/backend/routes/users.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 let User = require('../models/User.model');
 
-
+// get all users
 router.route('/').get((req, res)=>{
     User.find().then(users=> res.json(users)).catch(err=>res.status(400).json("Error: "+err));
 })
 
+// add user
 router.route('/create').post((req,res)=>{
     const username = req.body.username;
     const newUser = new User({username});
