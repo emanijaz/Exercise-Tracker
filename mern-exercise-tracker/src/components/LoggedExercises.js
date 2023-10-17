a4e10dc546935b10025c92c5965301516cee1506
emanFriendTales <eman@friendtales.com>
2023-10-17 21:49:04 +0500

    added delete and update integration of exercises

--- mern-exercise-tracker/src/components/LoggedExercises.js
+++ mern-exercise-tracker/src/components/LoggedExercises.js
@@ -1,9 +1,39 @@
-import React, { useEffect, useState, forwardRef, useImperativeHandle  } from 'react';
+import React, { useEffect, useState, forwardRef, useImperativeHandle, useRef } from 'react';
 import axios from 'axios'
 import dayjs from 'dayjs';
 import { EditOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Table, Button, Space, Popconfirm, message } from 'antd';
-  
+import { Table, Button, Space, Popconfirm, message, Modal, Form, Input, InputNumber } from 'antd';
+
+    
+const LoggedExercises = forwardRef((props, ref) => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [editModalVisible, setEditModalVisible] = useState(false);
+  const [editingExercise, setEditingExercise] = useState(null);
+  const editFormRef = useRef();
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get('http://localhost:5000/exercises/');
+      const d = response.data.map((exercise, index)=> ({
+        key: exercise._id,
+        description: exercise.description,
+        duration: exercise.duration,
+        date: dayjs(exercise.date).format('YYYY-MM-DD')
+      }))
+      setData(d);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []); 
+
   const columns = [
     {
       title: 'Description',
@@ -25,68 +55,70 @@ import { Table, Button, Space, Popconfirm, message } from 'antd';
       key: 'actions',
       render: (text, record) => (
       <Space size="middle">
-        <Button icon={<EditOutlined />} onClick={() => handleEdit(record.id)}>
-          Edit
-        </Button>
+        <Button icon={<EditOutlined />} onClick={() => handleEdit(record)}/>
         <Popconfirm
-          title="Delete the task"
-          description="Are you sure to delete this task?"
-          onConfirm={confirmDelete}
+          title="Delete the exercise"
+          description="Are you sure to delete this exercise?"
+          onConfirm={() => handleDelete(record)}
           onCancel={cancelDelete}
           okText="Yes"
           cancelText="No"
         >
-          <Button danger icon={<DeleteOutlined />} onClick={() => handleDelete(record.id)}>Delete</Button>
+          <Button danger icon={<DeleteOutlined />}/>
         </Popconfirm>
       </Space>
       )
     }
   ];
-  const confirmDelete = (e) => {
-    console.log(e);
-    message.success('Click on Yes');
-  };
   const cancelDelete = (e) => {
     console.log(e);
-    message.error('Click on No');
   };
-  const handleEdit = (id) => {
-    // Implement edit logic here
-    console.log(`Edit record with ID ${id}`);
-  };
-  
-  const handleDelete = (id) => {
-    // Implement delete logic here
-    console.log(`Delete record with ID ${id}`);
+  const handleEdit = (record) => {
+    console.log(record)
+    setEditingExercise(record);
+    setEditModalVisible(true);
   };
 
-  
-const LoggedExercises = forwardRef((props, ref) => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const handleEditModalCancel = () => {
+    setEditModalVisible(false);
+    setEditingExercise(null);
+  };
 
+  const handleEditModalOk = () => {
+    const form = editFormRef.current;
+    form
+      .validateFields()
+      .then(async (values) => {
+        try {
+          await axios.post(`http://localhost:5000/exercises/update/${editingExercise.key}`, {
+            username: "tester1",
+            description: values.description,
+            duration: Number(values.duration),
+            date: values.date,
+          });
+          message.success('Exercise updated successfully');
+          fetchData();
+        } catch (error) {
+          message.error('Error updating exercise');
+        } finally {
+          setEditModalVisible(false);
+          setEditingExercise(null);
+        }
+      })
+      .catch((error) => {
+        console.error('Validation error:', error);
+      });
+  };
 
-  const fetchData = async () => {
-    setLoading(true);
+  const handleDelete = async(record) => {
     try {
-      const response = await axios.get('http://localhost:5000/exercises/');
-      const d = response.data.map((exercise, index)=> ({
-        key: index,
-        description: exercise.description,
-        duration: exercise.duration,
-        date: dayjs(exercise.date).format('YYYY-MM-DD')
-      }))
-      setData(d);
+      await axios.delete(`http://localhost:5000/exercises/${record.key}`);
+      message.success('Exercise deleted successfully');
+      fetchData();
     } catch (error) {
-      console.error('Error fetching data:', error);
-    } finally {
-      setLoading(false);
-    }
+      message.error('Error deleting exercise');
+    } 
   };
-
-  useEffect(() => {
-    fetchData();
-  }, []); 
 
   // Expose the fetchData function through the ref
   useImperativeHandle(ref, () => ({
@@ -94,12 +126,45 @@ const LoggedExercises = forwardRef((props, ref) => {
   }));
 
   return (
+    <div>
     <Table
       columns={columns}
       dataSource={data}
       loading={loading}
       pagination={{ pageSize: 10 }}
     />
+
+    <Modal
+      title="Edit Exercise"
+      visible={editModalVisible}
+      onOk={handleEditModalOk}
+      onCancel={handleEditModalCancel}
+    >
+      <Form ref={editFormRef} initialValues={editingExercise?.record} layout="vertical">
+        <Form.Item
+          name="description"
+          label="Description"
+          rules={[{ required: true, message: 'Please enter the description' }]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="duration"
+          label="Duration"
+          rules={[{ required: true, message: 'Please enter the duration' }]}
+        >
+          <InputNumber min={1} />
+        </Form.Item>
+        <Form.Item
+          name="date"
+          label="Date"
+          rules={[{ required: true, message: 'Please select the date' }]}
+        >
+          <Input type="date" />
+        </Form.Item>
+      </Form>
+    </Modal>
+    </div>
   );
 });
 export default LoggedExercises;
